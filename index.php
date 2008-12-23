a2754ccd6a4708ce9edea836221d2bec817ac3c4
Paul Osborne <paul-osborne@bethel.edu>
2008-12-23 12:45:30 -0600

    Adding initial files to git repo

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Mango Movie Manager</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <link rel="shortcut icon" href="mango.png" type="image/x-icon" />
+</head>
+<body>
+<div id="wrap">
+<div id="header">
+    <h1 id="mango_title">Mango Movie Manager</h1>
+</div>
+<div id="main">
+<div id="content">
+<h2>Overview</h2>
+<p><div class="photobox" style="float:right"><a href="screenshot-main.png">
+      <img src="thumbnail-main.png" style="border:none" /></a><br /></div>
+  Mango is an advanced, yet easy to use movie management application.  With
+  Mango you can easily keep track and search through your personal movie
+  collection without having to go through all of your movies one at a time
+  trying to remember which ones you like or which movies Sean Connery acts in.  Mango
+  runs on all major operating systems and has a minimal, sleek user interface that
+  lets you manage your movies without getting in the way.
+</p>
+<h2>Features</h2>
+<p>Mango interfaces with its own, locally-hosted database and online web
+servies like Amazon to provide a rich variety of useful features:</p>
+<ul>
+    <li>Mango is cross-platform so it runs on Windows, Mac, and Linux.  In fact, it
+	will run on any computer that has Java Version 6.0 or higher installed.  Java
+	is free and available at <a href="http://java.com/getjava/">java.sun.com</a>.</li>
+    <li>Adding movies to Mango is easy.  Mango interfaces with Amazon's movie
+        database.  Just enter the title or ASIN of your movie and let Mango do
+        the rest!  Mango even downloads cover art for you movies!</li>
+    <li>Finding the movie you want in Mango is easy.  In Mango you can easily filter
+	the movies displayed in the main view table or you can create or execute an
+	advanced search on the movie database.  If the data is in the database, there
+	is a way to search for it.</li>
+    <li>Organizing the movies you have put in Mango is easy.  Mango allows you to create
+	sets of movies (collections that are not ordered and cannot have duplicates) and
+	lists of movies (which are like playlists that can have duplicates and have a defined
+	order).</li>
+    <li>Track who has your movies and whose movies you have.  We know that at any given time
+	friends, relatives, and all other manner of people might be borrowing your movies and
+	Mango lets you keep track of all that.  Then when you want to watch some missing movie
+	down the line you know who to hunt down.</li>
+</ul>
+<h2>Screenshots</h2>
+<p><a href="screenshot-main.png"><img src="thumbnail-main.png" /></a><br />
+<a href="screenshot-addeditmovie.png"><img src="thumbnail-addeditmovie.png" /></a><br />
+<a href="screenshot-savedsearch.png"><img src="thumbnail-savedsearch.png" /></a></p>
+</div>
+</div><!-- end body -->
+
+<div id="side">
+<div class="sidebar">
+   <h3>Get Mango!</h3>
+   <p>Mango is free and runs on any operating system with Java 6 installed. Get started
+	by downloading Mango now!
+   <h4>Download Mango!</h4>
+   <ul>
+	<li><a href="downloads/mango1.0-win32.exe">Mango for Windows</a></li>
+	<li><a href="downloads/mango1.0-osx.dmg">Mango for Mac</a></li>
+	<li><a href="downloads/mango1.0-linux.tar.gz">Mango for Linux</a></li>
+	<li><a href="downloads/mango1.0-java.jar">Mango Jar File</a></li>
+   </ul>
+   </p>
+</div>
+</div> <!-- end wrap -->
+
+</body>
+</html>
